10f8fb5804f9abd795aca775b398691506ab733e
Apurbo Deb Nath <apurbodebnath50@gmail.com>
2025-07-07 21:49:27 +0600

    done

---
+++ a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int maxTastiness = max(a, b) + max(c, d);
+        cout << maxTastiness << endl;
+    }
+
+    return 0;
+}
